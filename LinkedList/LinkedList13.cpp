100fd60c58f82156406788c835a6e9c149668873
Himanshu <hv65616@gmail.com>
2021-07-07 15:57:37 +0530

    Odd And Even Nodes in Linked List

---
+++ LinkedList/LinkedList13.cpp
@@ -0,0 +1,77 @@
+//To place all odd nodes together and all even nodes together
+//First we will place our odd nodes and then 
+//We will place our even nodes
+//Point to note is at last of odd node will join the starting of even nodes
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(Node *&head, int val)
+{
+    Node *n = new Node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void evenAndOddNodes(Node *&head)
+{
+    Node *odd = head;
+    Node *even = head->next;
+    Node *startEven = even;
+    while (odd->next != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = startEven;
+    if (odd->next != NULL)
+    {
+        even->next = NULL;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    display(head);
+    evenAndOddNodes(head);
+    display(head);
+    return 0;
+}
