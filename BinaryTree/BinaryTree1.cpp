dd9d875296dd053eee9bfabc3088b4cd5c442fef
Himanshu <hv65616@gmail.com>
2021-08-28 20:34:47 +0530

    Introduction to BT

---
+++ BinaryTree/BinaryTree1.cpp
@@ -0,0 +1,28 @@
+// Introduction to Binary Tree
+// How to create Node in BT and How to create branches in BT
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    return 0;
+}
