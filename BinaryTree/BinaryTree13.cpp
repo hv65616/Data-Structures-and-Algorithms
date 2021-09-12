80e126303c033e48d4fc799e09aaa8d182fda12b
Himanshu <hv65616@gmail.com>
2021-09-12 23:36:40 +0530

    Flatten Binary Tree

---
+++ BinaryTree/BinaryTree13.cpp
@@ -0,0 +1,62 @@
+//Flatten A Binary Tree
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void flatten(Node *root)
+{
+    if (root == NULL || (root->left == NULL && root->right == NULL))
+    {
+        return;
+    }
+    if (root->left != NULL)
+    {
+        flatten(root->right);
+        Node *temp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+
+        Node *t = root->right;
+        while (t->right != NULL)
+        {
+            t = t->right;
+        }
+        t->right = temp;
+    }
+    flatten(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(7);
+    flatten(root);
+    inorder(root);
+    cout << endl;
+    return 0;
+}
