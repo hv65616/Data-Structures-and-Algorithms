8243d1a915bd357741df14dd12d13b64aae91ed0
Himanshu <hv65616@gmail.com>
2022-05-31 01:24:08 +0530

    Reversing array using one pointer recursion

--- Recursion/Recursion15.cpp
+++ Recursion/Recursion15.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+//Reversing the array using one pointer
+#include <bits/stdc++.h>
 using namespace std;
+void rever(vector<int> &vt, int i, int n)
+{
+    if (i >= n/2)
+    {
+        return;
+    }
+    swap(vt[i], vt[n - i - 1]);
+    rever(vt, i + 1, n);
+}
 int main()
-{  
-    
+{
+    vector<int> vt = {1, 2, 3, 4, 5};
+    int n = vt.size();
+    rever(vt, 0, n);
+    for (auto i : vt)
+    {
+        cout << i << " ";
+    }
     return 0;
 }
