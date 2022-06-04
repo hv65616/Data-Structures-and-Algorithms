f236bb54c3920a0f459e7b797e83b598624da8b8
Himanshu <hv65616@gmail.com>
2022-06-04 17:38:42 +0530

    Sliding window problem 1

---
+++ Sliding Window/SW1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxsum(vector<int> &vt, int n, int k)
+{
+    int f3sum = 0;
+    for (int i = 0; i < k; i++)
+    {
+        f3sum += vt[i];
+        cout<<f3sum<<" ";
+    }
+    int wsum = f3sum;
+    for (int i = k; i < n; i++)
+    {
+        wsum += vt[i] - vt[i - k];
+        f3sum = max(wsum, f3sum);
+    }
+    return f3sum;
+}
+int main()
+{
+    vector<int> vt = {2, 5, 1, 8, 2, 9, 1};
+    int n = vt.size();
+    int k = 3;
+    int ans = maxsum(vt, n, k);
+    cout << ans << endl;
+    return 0;
+}
