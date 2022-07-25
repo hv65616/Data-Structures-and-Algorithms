56633a74516f122a0a1ca1438819d7301d361f84
Himanshu <hv65616@gmail.com>
2022-07-25 20:57:55 +0530

    Representation of graph

---
+++ Graph By Striver/Graph1.cpp
@@ -0,0 +1,50 @@
+// Undirected graph representation
+// Adjacency matrix representation
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int adj[n + 1][n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    return 0;
+}
+
+// Adjacency list representation
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
+
+// Adjacency list representation of weighted graph
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        adj[u].push_back({v, wt});
+        adj[v].push_back({u, wt});
+    }
+    return 0;
+}
